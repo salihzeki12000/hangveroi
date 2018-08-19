00e5aacc8e304269059af08a58644b3db5752a06
thebaoit <thebaoit@gmail.com>
2018-08-19 17:51:03 +0700

    Add google tags

--- resources/views/layouts/default.blade.php
+++ resources/views/layouts/default.blade.php
@@ -43,6 +43,13 @@
 	</script>
 	{!! App\Models\Setting::where('key', 'google_analytics_config')->first()["value"] !!}
 	<!-- Facebook Pixel Code -->
+	<!-- Google Tag Manager -->
+	<script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+	new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+	j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+	'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+	})(window,document,'script','dataLayer','GTM-M8VV393');</script>
+	<!-- End Google Tag Manager -->
 	<script>
 		!function(f,b,e,v,n,t,s)
 		{if(f.fbq)return;n=f.fbq=function(){n.callMethod?
@@ -61,6 +68,10 @@
 			<!-- End Facebook Pixel Code -->
 		</head>
 		<body>
+			<!-- Google Tag Manager (noscript) -->
+			<noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-M8VV393"
+			height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+			<!-- End Google Tag Manager (noscript) -->
 			<script>
 				window.fbAsyncInit = function() {
 					FB.init({
