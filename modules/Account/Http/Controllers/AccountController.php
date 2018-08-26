f3fbc7d1baa2d35f318ead19f0bb0e1ef2a5b37e
thebaoit <thebaoit@gmail.com>
2018-08-26 22:17:52 +0700

    Account login

--- modules/Account/Http/Controllers/AccountController.php
+++ modules/Account/Http/Controllers/AccountController.php
@@ -20,7 +20,7 @@ class AccountController extends Controller {
 
 	public function __construct()
     {
-        $this->middleware('auth');
+        $this->middleware('auth', ['except' => ['login', 'dologin', 'register', 'doregister']]);
     }
 
 	public function index()
