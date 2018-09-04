2c31257324a90a80137d427497c4bdb3b98b8f8b
thebaoit <thebaoit@gmail.com>
2018-09-04 22:18:33 +0700

    fix detail page

--- resources/views/modules/product/detail.blade.php
+++ resources/views/modules/product/detail.blade.php
@@ -70,9 +70,10 @@
 					<div class="col-md-5">
 						<div class="product-name">{{ $articleItem['name'] }}</div>
 						@if($articleItem->hasPromotion())
-						<b class="price margin-top-0 margin-bottom-5">{{ product_price($articleItem->getPromotion->money_has_discount) }}</b>&nbsp;<i class="real-price">{{ product_price($articleItem['price']) }}</i><br>
+						<p class="margin-bottom-0">Giảm giá: <b>{{ $articleItem->getPromotion->discount }}%</b></p>
+						<b class="price margin-top-0 margin-bottom-5 font-size-30">{{ product_price($articleItem->getPromotion->money_has_discount) }}</b>&nbsp;<i class="real-price font-size-12">{{ product_price($articleItem['price']) }}</i><br>
 						@else
-						<b class="price margin-top-0 margin-bottom-5">{{ product_price($articleItem['price']) }}</b><br>
+						<b class="price margin-top-0 margin-bottom-5 font-size-30">{{ product_price($articleItem['price']) }}</b><br>
 						@endif
 						<div class="boxcount-social">
 							<div class="fb-like" data-href="{{ Request::url() }}" data-layout="button_count" data-action="like" data-size="small" data-show-faces="false" data-share="true"></div> <br>
